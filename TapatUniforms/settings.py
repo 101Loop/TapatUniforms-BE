769f38a7296898e903bdbb7534fe923e66125dfd
Himanshu Shankar <me@himanshus.com>
2019-07-16 07:36:25 +0530

    Added `AWS CodeBuild` integration

--- TapatUniforms/settings.py
+++ TapatUniforms/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
-from .local_settings import *
 from .custom_settings import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,6 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '$%!n9vuqny(3)8ds817h!9z_qo2cc*1ptwuhd2$@%(1emtrlc_'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
