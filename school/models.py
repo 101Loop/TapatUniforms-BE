f7f743f31b70cad464b5b9b5dbdde136177a077f
Vivek Kaushik <kaushikvivek.17@gmail.com>
2019-07-18 18:18:04 +0530

    Vivek/school api fixed (#10)
    * added migration, fixed school api, removed extra fields from outlet
    
    * added migration
    
    * Added Extra fields to school model

--- school/models.py
+++ school/models.py
@@ -3,6 +3,8 @@ from django.utils.text import gettext_lazy as _
 
 from drfaddons.models import CreateUpdateModel
 
+from TapatUniforms.static_var import GENDER_CHOICES
+
 
 class School(CreateUpdateModel):
     name = models.CharField(verbose_name=_("School Name"), max_length=254)
@@ -22,6 +24,12 @@ class School(CreateUpdateModel):
 class Student(CreateUpdateModel):
     student_id = models.CharField(verbose_name=_("Student ID"), max_length=100)
     name = models.CharField(verbose_name=_("Name"), max_length=254)
+    standard = models.CharField(verbose_name=_("Class"), max_length=5)
+    section = models.CharField(verbose_name=_("Section"), max_length=5)
+    father_name = models.CharField(verbose_name=_("Father's Name"),
+                                   max_length=255)
+    gender = models.CharField(verbose_name=_("Geneder"),
+                              choices=GENDER_CHOICES, max_length=1)
     email = models.CharField(verbose_name=_("Email"), max_length=254)
     mobile = models.CharField(verbose_name=_("Contact no."), max_length=20)
     school = models.ForeignKey(to=School, verbose_name=_("School"),
